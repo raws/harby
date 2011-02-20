9d5374650064de82618c289669d9b900ac066235
Ross Paffett <ross@rosspaffett.com>
2011-02-20 14:51:04 -0500

    Add support for single-quoted strings

--- test/unit/arguments_parser_test.rb
+++ test/unit/arguments_parser_test.rb
@@ -17,20 +17,54 @@ module Harby
       assert_not_parsed " foo "
     end
     
-    test "a single naked string" do
+    test "mixed string identifiers should not parse" do
+      assert_not_parsed "'foo'bar"
+    end
+    
+    test "a naked string argument" do
       assert_parsed "foo", ["foo"]
       assert_parsed "foo\\ ", ["foo "]
       assert_parsed "\\ foo", [" foo"]
       assert_parsed "foo\\ \\ ", ["foo  "]
       assert_parsed "foo\\ bar", ["foo bar"]
+      assert_parsed 'foo"bar"', ['foo"bar"']
+      assert_parsed "foo'bar'", ["foo'bar'"]
+      assert_parsed "'foo\"bar", ["'foo\"bar"]
     end
     
-    test "multiple naked strings" do
+    test "multiple naked string arguments" do
       assert_parsed "foo bar", ["foo", "bar"]
       assert_parsed "foo  bar", ["foo", "bar"]
       assert_parsed "foo\\ bar baz", ["foo bar", "baz"]
       assert_parsed "foo bar  baz", ["foo", "bar", "baz"]
       assert_parsed "foo bar \\ baz", ["foo", "bar", " baz"]
+    end
+    
+    test "a single-quoted string argument" do
+      assert_parsed "'foo'", ["foo"]
+      assert_parsed "'foo '", ["foo "]
+      assert_parsed "' foo'", [" foo"]
+      assert_parsed "'foo bar'", ["foo bar"]
+      assert_parsed "'foo\\'bar'", ["foo'bar"]
+    end
+    
+    test "multiple single-quoted string arguments" do
+      assert_parsed "'foo' 'bar'", ["foo", "bar"]
+      assert_parsed "'foo bar' 'baz'", ["foo bar", "baz"]
+    end
+    
+    test "a single double-quoted string" do
+      skip "Not yet implemented"
+      assert_parsed '"foo"', ["foo"]
+      assert_parsed '"foo bar"', ["foo bar"]
+      assert_parsed '" foo"', [" foo"]
+      assert_parsed '"foo "', ["foo "]
+    end
+    
+    test "mixed string arguments" do
+      assert_parsed "foo 'bar baz'", ["foo", "bar baz"]
+      assert_parsed "foo\\ bar 'baz\\'oo'", ["foo bar", "baz'oo"]
+      assert_parsed "foo ' bar ' baz\\ ", ["foo", " bar ", "baz "]
     end
     
     def assert_parsed(input, expected)
