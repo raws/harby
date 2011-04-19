9ec27b460cc6611cd2e0fa944a877f531dcfef2e
Ross Paffett <ross@rosspaffett.com>
2011-04-19 01:22:59 -0400

    Add support for floating point numeric arguments

--- test/unit/numeric_parser_test.rb
+++ test/unit/numeric_parser_test.rb
@@ -2,10 +2,32 @@ require "test_helper"
 
 module Harby
   class NumericParserTest < ParserTestCase
+    test "a float argument" do
+      assert_parsed "12.3", [12.3]
+      assert_parsed "+12.3", [12.3]
+      assert_parsed "-12.3", [-12.3]
+      assert_parsed "0.0", [0.0]
+    end
+    
+    test "an invalid float argument" do
+      assert_parsed "--12.3", ["--12.3"]
+      assert_parsed "++12.3", ["++12.3"]
+    end
+    
+    test "multiple float arguments" do
+      assert_parsed "12.3 4.56", [12.3, 4.56]
+      assert_parsed "-12.3 4.56", [-12.3, 4.56]
+      assert_parsed "12.3 -4.56", [12.3, -4.56]
+      assert_parsed "+12.3 4.56", [12.3, 4.56]
+      assert_parsed "12.3 +4.56", [12.3, 4.56]
+      assert_parsed "-12.3 +4.56", [-12.3, 4.56]
+    end
+    
     test "an integer argument" do
       assert_parsed "123", [123]
       assert_parsed "-123", [-123]
       assert_parsed "+123", [123]
+      assert_parsed "0", [0]
     end
     
     test "an invalid integer argument" do
@@ -20,6 +42,13 @@ module Harby
       assert_parsed "123 +456", [123, 456]
       assert_parsed "+123 456", [123, 456]
       assert_parsed "-123 +456", [-123, 456]
+    end
+    
+    test "mixed numeric arguments" do
+      assert_parsed "123 4.56", [123, 4.56]
+      assert_parsed "12.3 456", [12.3, 456]
+      assert_parsed "-12.3 +456", [-12.3, 456]
+      assert_parsed "1 23.4 5", [1, 23.4, 5]
     end
   end
 end
