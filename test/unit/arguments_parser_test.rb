92e2613634d77b66f808fc8c3c45b1ee1e761c23
Ross Paffett <ross@rosspaffett.com>
2011-04-19 22:51:04 -0400

    Flesh out command argument tests

--- test/unit/arguments_parser_test.rb
+++ test/unit/arguments_parser_test.rb
@@ -8,6 +8,9 @@ module Harby
       assert_parsed "5 '5' 5.0 '5.0'", [5, "5", 5.0, "5.0"]
       assert_parsed '"5" 5 "5.0" 5.0', ["5", 5, "5.0", 5.0]
       assert_parsed "5\\ 0 5 5.0 5\\ .\\ 0", ["5 0", 5, 5.0, "5 . 0"]
+      assert_parsed "foo [bar 5]", ["foo", {:name => "bar", :args => [5]}]
+      assert_parsed "foo [bar 5.0]", ["foo", {:name => "bar", :args => [5.0]}]
+      assert_parsed "[foo bar 123 'baz oof']", [{:name => "foo", :args => ["bar", 123, "baz oof"]}]
     end
   end
 end
--- test/unit/command_parser_test.rb
+++ test/unit/command_parser_test.rb
@@ -3,7 +3,15 @@ require "test_helper"
 module Harby
   class CommandParserTest < ParserTestCase
     test "a command argument" do
-      assert_parsed "[foo bar] baz", [{:name => "foo", :args => ["bar"]}, "baz"]
+      assert_parsed "[foo]", [{:name => "foo", :args => []}]
+      assert_parsed "[foo bar]", [{:name => "foo", :args => ["bar"]}]
+      assert_parsed "[foo\\ bar]", [{:name => "foo bar", :args => []}]
+    end
+    
+    test "nested command arguments" do
+      assert_parsed "[foo bar [baz]]", [{:name => "foo", :args => ["bar", {:name => "baz", :args => []}]}]
+      assert_parsed "[foo bar [baz oof]]", [{:name => "foo", :args => ["bar", {:name => "baz", :args => ["oof"]}]}]
+      assert_parsed "[[foo]]", [{:name => {:name => "foo", :args => []}, :args => []}]
     end
   end
 end
